e268b0d86e4fb5f9e9b093786452f09746db0586
mikel evins <mevins@me.com>
2017-02-09 15:46:31 -0600

    added a second snake and control keys

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ A simple Snake game in Haxe
 11:50AM - started work on the program; experimenting with flash API
 12:44AM - ditched Atom to use Emacs; built simple Sprite that knows how to draw an apple
 1:15 - lunch break
-2:45 - adding features to snake
+2:15 - adding features to snake
+3:15 - break to walk the poodle
+3:30 - adding more snakes
+
 
--- Snake.hx
+++ Snake.hx
@@ -8,26 +8,43 @@ class Snake extends Sprite {
   public static var ORANGE = 0xCC6600;
   public static var YELLOW = 0xCCCC00;
   public static var GREEN = 0x00CC00;
+  public static var CYAN = 0x00CCCC;
   public static var BLUE = 0x0000CC;
   public static var PURPLE = 0xCC00CC;
   // size of the game board's implicit grid
   public static var scale = 16;
 
   private var theApple: Sprite;
-  private var theSnake: Sprite;
+  private var greenSnake: Sprite;
+  private var cyanSnake: Sprite;
+
   private var isInitialized = false;
   // the unique apple
 
   private function keyUp (event:KeyboardEvent){
     switch( event.keyCode ) {
+      // greenSnake
     case 38: // KEY_UP
-      theSnake.y -= scale;
+      greenSnake.y -= scale;
     case 40: // KEY_DOWN
-      theSnake.y += scale;
+      greenSnake.y += scale;
     case 39: // KEY_RIGHT
-      theSnake.x += scale;
+      greenSnake.x += scale;
     case 37: // KEY_LEFT
-      theSnake.x -= scale;
+      greenSnake.x -= scale;
+
+      // cyanSnake
+    case 87: // w
+      cyanSnake.y -= scale;
+    case 83: // s
+      cyanSnake.y += scale;
+    case 68: // d
+      cyanSnake.x += scale;
+    case 65: // s
+      cyanSnake.x -= scale;
+
+    default:
+      trace(event.keyCode);
     }
   };
 
@@ -40,17 +57,21 @@ class Snake extends Sprite {
     theApple.graphics.drawCircle(0, 0, scale/2);
     theApple.x = (stage.stageWidth - scale) / 2;
     theApple.y = (stage.stageHeight - scale) / 2;
-
     Lib.current.stage.addChild(theApple);
 
-    theSnake = new Sprite();
-    theSnake.graphics.beginFill(GREEN);
-    theSnake.graphics.drawCircle(0, 0, scale/2);
-    theSnake.x = (stage.stageWidth - scale) / 2;
-    theSnake.y = (stage.stageHeight - scale) / 2;
+    greenSnake = new Sprite();
+    greenSnake.graphics.beginFill(GREEN);
+    greenSnake.graphics.drawCircle(0, 0, scale/2);
+    greenSnake.x = (stage.stageWidth - scale) / 2;
+    greenSnake.y = (stage.stageHeight - scale) / 2;    
+    Lib.current.stage.addChild(greenSnake);
 
-    Lib.current.stage.addChild(theSnake);
-    
+    cyanSnake = new Sprite();
+    cyanSnake.graphics.beginFill(CYAN);
+    cyanSnake.graphics.drawCircle(0, 0, scale/2);
+    cyanSnake.x = (stage.stageWidth - scale) / 2;
+    cyanSnake.y = (stage.stageHeight - scale) / 2;    
+    Lib.current.stage.addChild(cyanSnake);    
 
     Lib.current.stage.addEventListener( KeyboardEvent.KEY_UP, keyUp );
   };
