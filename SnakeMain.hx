2a6228b6807cd8538524fedaa8c118ec58de3d7e
mikel evins <mevins@me.com>
2017-02-12 12:58:24 -0600

    new build file and main source file

---
+++ SnakeMain.hx
@@ -0,0 +1,14 @@
+import flash.Lib;
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.KeyboardEvent;
+
+class SnakeMain {
+  // main entry point
+  // ----------------
+  public static function main() 
+  {
+    Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+    Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+  };
+}
